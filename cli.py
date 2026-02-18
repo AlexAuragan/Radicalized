2cc6717460bc1226fb3e9478a11982ed15aa2fb9
AlexAuragan <alexauragan@gmail.com>
2026-02-18 20:13:33 +0100

    fix: contacts

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-#!/home/clawd/skills/radicale/scripts/Radicalized/.venv/bin/python
+#!~/clawd/skills/radicale/scripts/Radicalized/.venv/bin/python
 import argparse
 import sys
 from datetime import datetime
